beb3a96a74ac94cff5bf7c51d899944d18faf0d9
mickavnd <73641535+mickavnd@users.noreply.github.com>
2021-02-18 12:10:09 +0100

    cree varial global

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # projet-my-sql-11-02-21
 procedure:
 1.cree la base  de donnee avec la base de donnee sql
+2.cree un ficher .env et mettre a jour vos donnee de connection
+DB_HOST=
+DB_USER=
+DB_PASS=n
+DB_NAME=
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const app = express()
 const path =require('path')
+const mysql =require('mysql')
+const util = require('util')
+
+//dotenv
+require('dotenv').config()
+
+//mysql
+const db =mysql.createConnection(
+    {
+        host:process.env.DB_HOST,
+        user:process.env.DB_USER,
+        password:process.env.DB_PASS,
+        database:process.env.DB_NAME
+
+    }
+)
+db.connect(
+    (err)=>{
+        if (err){throw err}
+        console.log('connecter au server sql');
+    }
+)
+//variable global querysql
+global.querysql=util.promisify(db.query).bind(db)
+
 
 //EJS
 app.set('view engine','ejs');
